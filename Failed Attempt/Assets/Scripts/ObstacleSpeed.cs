5848a48a49114a88c6f8881b3568ae938fb47555
Mees Mijnheer <meespepijn@gmail.com>
2025-03-11 23:28:44 +0100

    Added an increase in speed to the objects, but it doesn't work yet.

--- Failed Attempt/Assets/Scripts/ObstacleSpeed.cs
+++ Failed Attempt/Assets/Scripts/ObstacleSpeed.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ObstacleSpeed : MonoBehaviour
 {
     public float obstacleSpeed = 5;
+    public float speedIncrease = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,7 @@ public class ObstacleSpeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        obstacleSpeed += Time.deltaTime * speedIncrease;
         transform.Translate(Vector2.left * obstacleSpeed * Time.deltaTime);
     }
 }
